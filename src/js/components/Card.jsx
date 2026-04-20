552241266e9bcd734bb2cc7bae03edf82f5cc37d
lainneker-kairos <lainneker@gmail.com>
2026-04-20 16:20:36 +0000

    lnkr126destructutigCoding_myFirtsComponent

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,16 +1,16 @@
-const Card = (props) => {
-	return (
-		<div className="card h-100">
-			<img src={props.imageUrl} className="card-img-top" alt="..." />
-			<div className="card-body text-center">
-				<h5 className="card-title">{props.title}</h5>
-				<p className="card-text">{props.description}</p>
-			</div>
-			<div className="card-footer text-center bg-transparent border-top-0">
-				<a href="#" className="btn btn-primary">{props.buttonLabel}</a>
-			</div>
-		</div>
-	);
+const Card = ({ imageUrl, title, description, buttonLabel }) => {
+  return (
+    <div className="card h-100">
+      <img src={imageUrl} className="card-img-top" alt={title} />
+      <div className="card-body text-center">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+      </div>
+      <div className="card-footer text-center bg-transparent border-top-0">
+        <a href="#" className="btn btn-primary">{buttonLabel}</a>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,62 +7,56 @@ import Jumbotron from "./Jumbotron";
 
 //create your first component
 const cardsData = [
-    {
-	      imageUrl: "https://picsum.photos/id/10/500/325",
-		        title: "Tarjeta 1",
-				      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.",
-					        buttonLabel: "VER MÁS!"
-							    },
-								    {
-									      imageUrl: "https://picsum.photos/id/20/500/325",
-										        title: "tarjeta 2",
-												      description: "Explicabo magnam fugit commodi assumenda veniam obcaecati in quia aut mollitia.",
-													        buttonLabel: "VER MÁS!"
-															    },
-																    {
-																	      imageUrl: "https://picsum.photos/id/30/500/325",
-																		        title: "tarjeta 3",
-																				      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.",
-																					        buttonLabel: "VER MÁS!"
-																							    },
-																								    {
-																									      imageUrl: "https://picsum.photos/id/40/500/325",
-																										        title: "Tarjeta 4",
-																												      description: "Explicabo magnam fugit commodi assumenda veniam obcaecati in quia aut mollitia.",
-																													        buttonLabel: "VER MÁS!"
-																															    }
-																																  ];
+  {
+    imageUrl: "https://picsum.photos/id/10/500/325",
+    title: "Tarjeta 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.",
+    buttonLabel: "VER MÁS!"
+  },
 
-																																  const Home = () => {
-																																  	
-																																	return (
-																																	    <div className="d-flex flex-column min-vh-100">
-																																		      <Nav />
-																																			        
-																																					      
-																																						        <div className="container flex-grow-1 mb-5">
-																																								        <Jumbotron />
-																																										        
-																																												        
-																																														        <div className="row">
-																																																          
-																																																		            {cardsData.map((card, index) => (
-																																																					            <div className="col-lg-3 col-md-6 mb-4" key={index}>
-																																																								              <Card 
-																																																											                  imageUrl={card.imageUrl} 
-																																																															                  title={card.title} 
-																																																																			                  description={card.description} 
-																																																																							                  buttonLabel={card.buttonLabel} 
-																																																																											                />
-																																																																															            </div>
-																																																																																		          ))}
-																																																																																				          </div>
-																																																																																						        </div>
+  {
+    imageUrl: "https://picsum.photos/id/20/500/325",
+    title: "tarjeta 2",
+    description: "Explicabo magnam fugit commodi assumenda veniam obcaecati in quia aut mollitia.",
+    buttonLabel: "VER MÁS!"
+  },
 
-																																																																																								      <Footer />
-																																																																																									      </div>
-																																																																																										    );
+  {
+    imageUrl: "https://picsum.photos/id/30/500/325",
+    title: "tarjeta 3",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.",
+    buttonLabel: "VER MÁS!"
+  },
 
-																																																																																											};
+  {
+    imageUrl: "https://picsum.photos/id/40/500/325",
+    title: "Tarjeta 4",
+    description: "Explicabo magnam fugit commodi assumenda veniam obcaecati in quia aut mollitia.",
+    buttonLabel: "VER MÁS!"
+  }
+];
 
-																																																																																											export default Home;
+const Home = () => {
+  return (
+    		<div className="d-flex flex-column min-vh-100">
+      		<Nav />
+
+      		<div className="container flex-grow-1 mb-5 mt-4">
+      		<Jumbotron />
+
+      		<div className="row">
+               {cardsData.map((card, index) => (
+     		 <div className="col-lg-3 col-md-6 mb-4" key={index}>
+
+    		 <Card {...card} />
+            </div>
+         	 ))}
+       		 </div>
+      		</div>
+
+    		  <Footer />
+     		 </div>
+     );
+};
+
+export default Home;
